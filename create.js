ff0f643cd5fbfdeb43e6a5aa55460d9b03ac9815
jbury15 <43784680+jbury15@users.noreply.github.com>
2019-03-28 10:15:42 -0400

    added functions

--- create.js
+++ create.js
@@ -1,1 +1,43 @@
-
+function check(){
+  var question 1 = document.quiz.question1.value;
+  var question 2 = document.quiz.question1.value;
+  var question 3 = document.quiz.question1.value;
+  var question 4 = document.quiz.question1.value;
+  var question 5 = document.quiz.question1.value;
+  var question 6 = document.quiz.question1.value;
+  var question 7 = document.quiz.question1.value;
+  var question 8 = document.quiz.question1.value;
+  var question 9 = document.quiz.question1.value;
+  var question 10 = document.quiz.question1.value;
+  var correct = 0;
+  
+     if (question1 == "4") {
+       correct++;
+     }
+    if (question2 == "6") {
+       correct++;
+     }
+  if (question3 == "0") {
+       correct++;
+     }
+if (question4 == "5") {
+       correct++;
+     }
+  if (question5 == "0") {
+       correct++;
+     }
+  if (question6 == "5") {
+       correct++;
+     }
+  if (question7 == "1") {
+       correct++;
+     }
+  if (question8 == "0") {
+       correct++;
+     }
+  if (question9 == "1") {
+       correct++;
+     }
+  if (question10== "3") {
+       correct++;
+     }
