f5a60830fd8dd72b02ffb50ae9e7fc0cc98490a3
jbury15 <43784680+jbury15@users.noreply.github.com>
2019-04-01 10:16:25 -0400

    fixed variable name

--- create.js
+++ create.js
@@ -1,14 +1,14 @@
 function check(){
-  var question 1 = document.quiz.question1.value;
-  var question 2 = document.quiz.question2.value;
-  var question 3 = document.quiz.question3.value;
-  var question 4 = document.quiz.question4.value;
-  var question 5 = document.quiz.question5.value;
-  var question 6 = document.quiz.question6.value;
-  var question 7 = document.quiz.question7.value;
-  var question 8 = document.quiz.question8.value;
-  var question 9 = document.quiz.question9.value;
-  var question 10 = document.quiz.question10.value;
+  var question1 = document.quiz.question1.value;
+  var question2 = document.quiz.question2.value;
+  var question3 = document.quiz.question3.value;
+  var question4 = document.quiz.question4.value;
+  var question5 = document.quiz.question5.value;
+  var question6 = document.quiz.question6.value;
+  var question7 = document.quiz.question7.value;
+  var question8 = document.quiz.question8.value;
+  var question9 = document.quiz.question9.value;
+  var question10 = document.quiz.question10.value;
   var correct = 0;
   
      if (question1 == "4") {
