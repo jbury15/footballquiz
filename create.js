5c948cfa18b6270c773666c592c5a356829001b2
jbury15 <43784680+jbury15@users.noreply.github.com>
2019-04-03 09:56:22 -0400

    removed .value

--- create.js
+++ create.js
@@ -1,14 +1,14 @@
 function check(){
   var question1 = document.quiz.question1;
-  var question2 = document.quiz.question2.value;
-  var question3 = document.quiz.question3.value;
-  var question4 = document.quiz.question4.value;
-  var question5 = document.quiz.question5.value;
-  var question6 = document.quiz.question6.value;
-  var question7 = document.quiz.question7.value;
-  var question8 = document.quiz.question8.value;
-  var question9 = document.quiz.question9.value;
-  var question10 = document.quiz.question10.value;
+  var question2 = document.quiz.question2;
+  var question3 = document.quiz.question3;
+  var question4 = document.quiz.question4;
+  var question5 = document.quiz.question5;
+  var question6 = document.quiz.question6;
+  var question7 = document.quiz.question7;
+  var question8 = document.quiz.question8;
+  var question9 = document.quiz.question9;
+  var question10 = document.quiz.question10;
   var correct = 0;
   
      if (question1 == "4") {
